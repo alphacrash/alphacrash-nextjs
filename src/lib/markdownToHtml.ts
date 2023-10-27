0459899af6e2dff64d5e1cd034b8151be743c065
alphacrash <sudhansu.ace@gmail.com>
2023-10-27 14:20:16 +0530

    Updated Code Block Styles

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,7 +1,12 @@
 import { remark } from 'remark'
 import html from 'remark-html'
+import rehypeHighlight from 'rehype-highlight'
 
 export default async function markdownToHtml(markdown: string) {
-    const result = await remark().use(html).process(markdown)
+    const result = await remark()
+        .use(html)
+        .use(rehypeHighlight)
+        .process(markdown)
+
     return result.toString()
 }
